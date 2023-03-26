5bf9ec4bc915aeaccd5968e078898a48aaba897b
muhaimin <muhaimin@aoneschools.com>
2023-03-26 17:17:30 +0800

    control active style to navbar upon scrolling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
 
-window.onscroll(() => {
+window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
@@ -16,4 +16,4 @@ window.onscroll(() => {
       });
     }
   });
-});
+};
